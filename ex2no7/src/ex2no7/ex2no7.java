58d0226ebcadcc73d17f5bc061458fc9b964d39a
admin <admin@DESKTOP-8L2IG92>
2021-03-30 19:33:05 +0900

    ex2no7

---
+++ ex2no7/src/ex2no7/ex2no7.java
@@ -0,0 +1,15 @@
+package ex2no7;
+
+public class ex2no7 { //56p
+
+	 public static void main(String[] args) {
+		char ch = 'A';       
+		int code = (int)ch;  
+		
+		System.out.printf("%c = %d(%#X)%n",ch,code,code);
+		 
+		char hch = '°¡';      
+		System.out.printf("%c = %d(%#X)%n", hch, (int)hch, (int)hch);
+	} 
+	
+}
